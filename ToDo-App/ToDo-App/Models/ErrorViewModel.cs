478962d6db4794279dc2301e41f3f5a57fb06f0f
toshly24 <113390335+toshly24@users.noreply.github.com>
2022-11-16 02:44:51 -0500

    Added the model and the DbContext class

---
+++ ToDo-App/ToDo-App/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ToDo_App.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
---
+++ ToDo-App/ToDo-App/Models/ToDoList.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ToDo_App.Models
+{
+    public class ToDoList
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Content { get; set; }
+
+    }
+}
+
