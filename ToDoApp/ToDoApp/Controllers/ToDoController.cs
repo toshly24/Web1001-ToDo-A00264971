2f734fce1bc47a96ff9e56b8b84a70f430260c2f
toshly24 <113390335+toshly24@users.noreply.github.com>
2022-11-17 14:37:57 -0500

    Added the model class

---
+++ ToDoApp/ToDoApp/Controllers/ToDoController.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using ToDoApp.Models;
+
+namespace ToDoApp.Controllers;
+
+public class ToDoController : Controller
+{
+    private readonly ILogger<ToDoController> _logger;
+
+    public ToDoController(ILogger<ToDoController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
+
